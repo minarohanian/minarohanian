8a2410df0d9284fabc1f5880542403e72ae2bcd8
minarohanian <rohanian.mina@gmail.com>
2024-06-16 14:44:45 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
 
+if (document.getElementById('my-work-link')) {
+  document.getElementById('my-work-link').addEventListener('click', () => {
+    document.getElementById('my-work-section').scrollIntoView({behavior: "smooth"})
+  })
+}
